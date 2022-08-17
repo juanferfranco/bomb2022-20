52a646addc91a22bdb33dc9c9f458e48512c0661
Juan UPB Linux kraken <juanf.franco@upb.edu.co>
2022-08-17 16:56:44 -0500

    Init dev project

--- task1.cpp
+++ task1.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
 #include "task1.h"
 
-
 void task1(){
+    enum class TaskState{INIT, WAIT_COMMANDS};
+    static TaskState taskState = TaskState::INIT;
 
-    
+    switch(taskState){
+        case TaskState::INIT:{
+            Serial.begin(115200);
+            taskState = TaskState::WAIT_COMMANDS;
+            break;
+        }
+        case TaskState::WAIT_COMMANDS:{
+            if(Serial.available() > 0){
+                int dataRx = Serial.read();
+                if(dataRx == 'u'){
+                    Serial.print("UP BTN\n");
+                }
+            }
+            break;
+        }
+        default:{
+        }
+    } 
+   
 }
--- task2.h
+++ task2.h
@@ -1,6 +1,7 @@
 #ifndef _TASK2_H_
 #define _TASK2_H_
 
+
 void task2();
 
 #endif
